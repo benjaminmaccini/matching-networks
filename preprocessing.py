2076ec851015c7eead8b809d6a15dd62489d38d4
Ben <benjaminmaccini@gmail.com>
2019-04-08 11:36:18 -0500

    Auth working 100%

--- preprocessing.py
+++ preprocessing.py
@@ -6,9 +6,15 @@ import argparse
 import numpy as np
 from pprint import pprint
 
+import pandas as pd
+
+import pickle
+
 import pdb
 
 '''
+  	python3 preprocessing.py --list songs.txt artists.txt
+	
   Features (12):
   	id
 	acousticness
@@ -23,78 +29,108 @@ import pdb
 	tempo
 	time_signature
 	valence
+
+	timbre *** TODO see audio_analysis
 '''
+class Song:
+	'''
+		Song Data Layer
+	'''
 
-CLIENT_ID = os.environ['CLIENT_ID']
-CLIENT_SECRET = os.environ['CLIENT_SECRET']
+	def __init__(self):
+		self.CLIENT_ID = os.environ['CLIENT_ID']
+		self.CLIENT_SECRET = os.environ['CLIENT_SECRET']
+		self.ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+		self.TOKEN_HEADER = {}
 
-ACCESS_TOKEN = ''
-TOKEN_HEADER = {}
+	def spotify_auth(self):
+		payload = {'grant_type':'client_credentials'}
+		r = requests.post('https://accounts.spotify.com/api/token', data=payload,
+										 auth=HTTPBasicAuth(self.CLIENT_ID, self.CLIENT_SECRET))
+		print("Token {}".format(r))
+		os.environ['ACCESS_TOKEN'] = r.json()['access_token']
+		self.ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+		self.TOKEN_HEADER = {'Authorization': 'Bearer ' + self.ACCESS_TOKEN}
+		print("Successfully retrieved token:")
+		print(self.TOKEN_HEADER)
 
-def spotify_auth():
-	payload = {'grant_type':'client_credentials'}
-	r = requests.post('https://accounts.spotify.com/api/token', data=payload,
-									 auth=HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET))
-	print("Token {}".format(r))
-	ACCESS_TOKEN = r.json()['access_token']
-	TOKEN_HEADER = {'Authorization': 'Bearer {}'.format(ACCESS_TOKEN)}
-	print("Successfully retrieved token:")
-	pprint(r.json())
-	print('\n~\n~\n~')
+	def get_artist_genre(self, artist_id):
+		r = requests.get("https://api.spotify.com/v1/artists/{}".format(artist_id), headers=self.TOKEN_HEADER)
+		print("{} for artist_id {}".format(r,artist_id))
+		data = r.json()['genres'] # List of Strings
+		name = r.json()['name']
+		return data, name
 
-def create_map():
-	# Utiliy script to compile all the genres and map them to prime numbers
-	# This is an open TODO... is this even the correct method to use???
-	pass
+	def get_song(self, song_id):
+		r = requests.get("https://api.spotify.com/v1/audio-features/{}".format(song_id), headers=self.TOKEN_HEADER)
+		print("{} for song_id {}".format(r,song_id))
+		data = r.json()
+		return data
 
-def get_artist_genre(song_id):
-	r = requests.get("https://api.spotify.com/v1/artists/{}".format(song_id), headers=TOKEN_HEADER)
-	data = r.json()['genres'] # List of Strings
-	name = r.json()['name']
-	return data, name
+	def collect(self, song_ids, artist_ids):
+		supp = []
+		for song_id, artist_id in zip(song_ids, artist_ids):
+			song_id = self.get_id(song_id)
+			artist_id = self.get_id(artist_id)
+			data = self.get_song(song_id)
+			labels, metalabel = self.get_artist_genre(artist_id)
+			temp = {metalabel:(labels,data)}
+			supp.append(temp)
+		return supp
 
-def get_song(song_id):
-	r = requests.get("https://api.spotify.com/v1/audio-features/{}".format(song_id), headers=TOKEN_HEADER)
-	print("{} for song_id {}".format(r,song_id))
-	data = r.json()
-	return data
+	def audio_analysis(self, song_id):
+		# This is a TODO because it requires a lot of extra work to vectorize
+		r = requests.get("https://api.spotify.com/v1/audio-analysis/{}".format(song_id), headers=self.TOKEN_HEADER)
+		print("{} for song_id {}".format(r,song_id))
+		data = r.json()
+		return data
 
-def collect(song_ids):
-	json_list = []
-	return json_list
+	def vectorize(self, supp):
+		song_features = np.zeros(12,1)
+		song_features = pd.get_dummies(song_features)
+		return song_features
 
-def vectorize(json_list):
-	song_features = np.zeros(12,1)
-	return song_features
+	def get_id(self, uri):
+		return uri.strip().split(':')[-1]
 
-def get_id_from_uri(uri):
-	return uri.strip().split(':')[-1]
+	def e2e(self):
+		# Take in a text file and then create support set
+		pass
 
 def parse_args():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--list', dest='collection', action='append', default=[], help='A list of Spotify uris or song ids')
-	parser.add_argument('--id', dest='id', default='foo')
+	parser.add_argument('--list', dest='collection', default=[], nargs='+', help='A list of Spotify uris or song ids')
+	parser.add_argument('--uri', dest='uri', default='foo')
 	parser.add_argument('--auth_test', dest='auth', action='store_true')
-	parser.add_argument('--create_map', dest='create_map', action='store_true')
 	parser.set_defaults(auth=False, create_map=False)
 
 	return parser.parse_args()
 
 
 if __name__ == '__main__':
+	s = Song()
 	args = parse_args()
 	try:
-		spotify_auth()
+		s.spotify_auth()
 	except Exception as e:
 		pprint(e)
 		sys.exit()
-
-	if(args.create_map):
-		artist_ids = [0,1,2,3,4]
-		create_map()
-	elif(args.collection != []):
-		json_list = collect(args.collection)
-		song_features = vectorize(json_list)
-		pprint(song_features)
-	elif(args.id != 'foo'):
-		pprint(get_song(args.id))
+	if(not args.auth):
+		if(args.collection != []):
+			song_list = []
+			with open(args.collection[0], 'r') as in_file:
+				for line in in_file:
+					song_list.append(line.strip())
+			artist_list = []
+			with open(args.collection[1], 'r') as in_file:
+				for line in in_file:
+					artist_list.append(line.strip())
+			supp = s.collect(song_list, artist_list)
+			# song_features = s.vectorize(supp)
+			pprint(song_features)
+		elif(args.uri != 'foo'):
+			song_id = s.get_id(args.uri)
+			pprint(s.get_song(song_id))
+			# s.audio_analysis(song_id)
+		else:
+			print("Check args")
